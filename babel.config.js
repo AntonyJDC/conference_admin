c09ef0545c47493321e51e6523718eb8d3e08eed
AntonyJDC <jaconsuegra@uninorte.edu.co>
2025-05-23 14:16:50 -0500

    refactor: simplify babel configuration by removing unused plugins and updating presets

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,17 @@
 module.exports = function (api) {
   api.cache(true);
-  let plugins = [];
-
   return {
-    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
-
-    plugins,
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          allowUndefined: true,
+        },
+      ],
+      'react-native-reanimated/plugin',
+    ],
   };
 };
