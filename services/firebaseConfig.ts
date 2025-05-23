85a4b2a5c7b59df1bc9194370b49ac7faf32c92c
AntonyJDC <jaconsuegra@uninorte.edu.co>
2025-05-23 14:25:23 -0500

    chore: refactor firebase configuration to use environment variables

--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -1,14 +1,23 @@
 import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID,
+} from '@env';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBHjuBDvz4cd2Bvimgac94S-2Yityfkh-4",
-    authDomain: "conference-6a0fd.firebaseapp.com",
-    projectId: "conference-6a0fd",
-    storageBucket: "conference-6a0fd.firebasestorage.app",
-    messagingSenderId: "679580191954",
-    appId: "1:679580191954:web:d25515147c71a98463f10b",
-    measurementId: "G-6J0HWCP7KB"
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
